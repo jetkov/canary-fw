ed03b9c8371b4f5a81d1dbc06cd0d3e36e9cd98e
Alex Petkovic <16639190+jetkov@users.noreply.github.com>
2021-04-01 19:59:28 -0400

    Init

---
+++ canary/Output/Release/Obj/ble_app_blinky_pca10040_s132/ble_conn_params.d
@@ -0,0 +1,68 @@
+Output/Release/Obj/ble_app_blinky_pca10040_s132/ble_conn_params.o: \
+ C\:\Users\Alex\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\Canary\nRF5SDK1702d674dde\components\ble\common\ble_conn_params.c \
+ ../nRF5SDK1702d674dde/components/libraries/util/sdk_common.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42/include/stdint.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42/include/stdbool.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42/include/string.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42/include/__crossworks.h \
+ config/sdk_config.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/nordic_common.h \
+ ../nRF5SDK1702d674dde/modules/nrfx/mdk/compiler_abstraction.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/sdk_os.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/sdk_errors.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/nrf_error.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/app_util.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42/include/stddef.h \
+ ../nRF5SDK1702d674dde/modules/nrfx/mdk/nrf.h \
+ ../nRF5SDK1702d674dde/modules/nrfx/mdk/nrf52.h \
+ ../nRF5SDK1702d674dde/components/toolchain/cmsis/include/core_cm4.h \
+ ../nRF5SDK1702d674dde/components/toolchain/cmsis/include/cmsis_version.h \
+ ../nRF5SDK1702d674dde/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../nRF5SDK1702d674dde/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../nRF5SDK1702d674dde/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../nRF5SDK1702d674dde/modules/nrfx/mdk/system_nrf52.h \
+ ../nRF5SDK1702d674dde/modules/nrfx/mdk/nrf52_bitfields.h \
+ ../nRF5SDK1702d674dde/modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../nRF5SDK1702d674dde/modules/nrfx/mdk/nrf52_name_change.h \
+ ../nRF5SDK1702d674dde/modules/nrfx/mdk/compiler_abstraction.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/nrf52/nrf_mbr.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/nrf_svc.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/sdk_macros.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/nrf_assert.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42/include/stdlib.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/sdk_errors.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_hci.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_err.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/nrf_error.h \
+ C\:\Users\Alex\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\Canary\nRF5SDK1702d674dde\components\ble\common\ble_conn_params.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/nrf_svc.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_err.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_gap.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_hci.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_ranges.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_types.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_l2cap.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_gatt.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_gattc.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_gatts.h \
+ C\:\Users\Alex\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\Canary\nRF5SDK1702d674dde\components\ble\common\ble_srv_common.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_types.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/app_util.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_gap.h \
+ ../nRF5SDK1702d674dde/components/softdevice/s132/headers/ble_gatt.h \
+ C\:\Users\Alex\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\Canary\nRF5SDK1702d674dde\components\ble\common\ble_conn_state.h \
+ ../nRF5SDK1702d674dde/components/libraries/atomic/nrf_atomic.h \
+ ../nRF5SDK1702d674dde/components/softdevice/common/nrf_sdh_ble.h \
+ ../nRF5SDK1702d674dde/components/libraries/experimental_section_vars/nrf_section_iter.h \
+ ../nRF5SDK1702d674dde/components/libraries/experimental_section_vars/nrf_section.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/nordic_common.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/nrf_assert.h \
+ ../nRF5SDK1702d674dde/components/libraries/timer/app_timer.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/app_error.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42/include/stdio.h \
+ ../nRF5SDK1702d674dde/components/libraries/util/app_error_weak.h \
+ ../nRF5SDK1702d674dde/components/libraries/log/nrf_log_instance.h \
+ ../nRF5SDK1702d674dde/components/libraries/experimental_section_vars/nrf_section.h \
+ ../nRF5SDK1702d674dde/components/libraries/log/nrf_log_types.h \
+ ../nRF5SDK1702d674dde/components/libraries/sortlist/nrf_sortlist.h
---
+++ canary/Output/Release/Obj/ble_app_blinky_pca10040_s132/utf.d
@@ -0,0 +1,6 @@
+Output/Release/Obj/ble_app_blinky_pca10040_s132/utf.o: \
+ C\:\Users\Alex\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\Canary\nRF5SDK1702d674dde\external\utf_converter\utf.c \
+ C\:\Users\Alex\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\Canary\nRF5SDK1702d674dde\external\utf_converter\utf.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42/include/stddef.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42/include/__crossworks.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42/include/stdint.h
